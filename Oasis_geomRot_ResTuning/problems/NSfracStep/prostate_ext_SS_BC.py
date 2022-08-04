88d119d396313be0932a7e882e89ada76fc6579a
MosBio <108097580+MosBio@users.noreply.github.com>
2022-08-04 16:19:52 -0700

    Add files via upload

---
+++ Oasis_geomRot_ResTuning/problems/NSfracStep/prostate_ext_SS_BC.py
@@ -0,0 +1,31 @@
+import numpy
+
+
+def time_BC():
+
+
+#P18 IR calculated from Paraview
+   time = numpy.array([
+      -0.019,
+			0.0,
+                        1,
+                     ])
+   return time
+
+
+def Vel_IR_BC():
+    
+    
+    V_IR = numpy.array([
+      10,
+      28.47e3,
+                        28.47e3,
+                          ])
+
+
+    return  V_IR
+
+
+
+
+
